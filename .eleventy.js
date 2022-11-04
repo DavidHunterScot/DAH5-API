1229ff46b93264c5516783906be103d0cc30c717
DavidHunterScot <github@davidhunter.scot>
2022-11-04 00:34:46 +0000

    Updated 11ty config to copy JSON files in the user directory to the output directory and added first user json file with basic data

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/user/*.json");
   
   return {
     dir: {
